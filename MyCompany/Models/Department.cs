2e509f6258efcb1a827d9bb48d8fefcb911dd421
vad79-79 <vad79-79@DESKTOP-114VD3K>
2023-02-26 20:16:37 +0200

    Добавьте файлы проекта.

---
+++ MyCompany/Models/Department.cs
@@ -0,0 +1,12 @@
+﻿namespace MyCompany.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string DepName { get; set; }
+
+
+        public virtual List<Employee>? Employees { get; set; }
+
+    }
+}
